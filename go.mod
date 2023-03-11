ae65752df64e6f99fe1353991a811a7b3503d931
514K <alekseysafronov0@gmail.com>
2023-03-12 01:09:11 +0300

    test pass

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/514K/testModles/mymodul
 
 go 1.20
+
+require github.com/514K/GolangLearn v0.0.0-20230311214757-0a7413482d63 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,12 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/514K/GolangLearn/path6/storage"
+)
+
+func main() {
+	sas := storage.Sotrudnik{}
+	fmt.Printf("%v\n", sas)
 }
